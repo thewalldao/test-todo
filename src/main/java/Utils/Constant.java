1480e40f4ff286f7b10c4ce71182cfcbdc10b227
tuan041 <thewalldao@gmail.com>
2022-07-04 19:57:25 +0700

    update core

--- src/main/java/Utils/Constant.java
+++ src/main/java/Utils/Constant.java
@@ -7,7 +7,6 @@ public class Constant {
     public static final int WAIT_SHORT_TIMEOUT = 5;
     public static final String ALLURE_RESULT_PATH = System.getProperty("user.dir") + "/allure-results";
     public static final String HUB_URL = "http://localhost:4444/wd/hub";
-    public static final String AUT_URL = "https://tiki.vn";
     public static final String DEFAULT_BROWSER = "chrome";
     public static final String DEFAULT_REMOTE_STATE = "false";
     public static final String DEFAULT_GITHUB_USERNAME = "tuan041.test.test@gmail.com";
